c2c54aeeae5898192c1767e3138f0da3204eeb9b
mdanish396 <danishazhar186@gmail.com>
2022-11-18 11:10:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/grid/ImageAdapter.java
@@ -0,0 +1,41 @@
+package com.example.grid;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public Integer[] thumbImages = {
+            R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5,
+            R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10,
+            R.drawable.pic11, R.drawable.pic12, R.drawable.pic13, R.drawable.pic14,
+            R.drawable.pic15,
+    };
+
+    public ImageAdapter(Context c){mContext = c; }
+
+    @Override
+    public int getCount(){return thumbImages.length; }
+
+    @Override
+    public Object getItem(int position){ return thumbImages[position]; }
+
+    @Override
+    public long getItemId(int position){ return 0; }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(250,250));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setPadding(8,8,8,8);
+        imageView.setImageResource(thumbImages[position]);
+        return imageView;
+    }
+
+}
